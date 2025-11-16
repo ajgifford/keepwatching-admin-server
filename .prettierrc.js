425922273c1d05b0e549d72d1852bfd5cef8356f
Andy Gifford <ajgifford@users.noreply.github.com>
2025-11-16 15:16:24 -0600

    Update dependencies and switch to ESM

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+export default {
+  singleQuote: true,
+  proseWrap: 'always',
+  printWidth: 120,
+  tabWidth: 2,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrder: ['dotenv', 'react', '<THIRD_PARTY_MODULES>'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
---
+++ jest.config.js
@@ -0,0 +1,37 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+    '^.+\\.jsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  collectCoverage: false,
+  collectCoverageFrom: ['src/**/*.ts', '!src/index.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  transformIgnorePatterns: ['node_modules/(?!(@ajgifford|uuid)/)'],
+  moduleNameMapper: {
+    '^vitest$': '<rootDir>/tests/mocks/vitest.ts',
+    '^@controllers/(.*)$': '<rootDir>/src/controllers/$1',
+    '^@middleware/(.*)$': '<rootDir>/src/middleware/$1',
+    '^@routes/(.*)$': '<rootDir>/src/routes/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
